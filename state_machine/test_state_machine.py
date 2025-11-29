b9dc2529c45df5c86ef0582ed00e191ee2bdca6a
mor4z <morazio.2018794@studenti.uniroma1.it>
2025-11-29 10:35:23 +0100

    state_machine_0

---
+++ state_machine/test_state_machine.py
@@ -0,0 +1,22 @@
+from state_machine import StateMachine
+
+# Testing
+if __name__ == '__main__':
+    sm = StateMachine()
+    
+    print(sm)  # Current state: q0
+    sm.transition()
+    print(sm)  # Current state: q1
+    sm.transition()
+    print(sm)  # Current state: q2
+    sm.transition()
+    print(sm)  # Current state: q3
+    sm.transition()
+    print(sm)  # Current state: q4
+    sm.transition()
+    print(sm)  # Current state: q5
+    sm.transition()
+    print(sm)  # Current state: q6
+    
+    sm2 = StateMachine('q4')
+    print(sm2) # Current state: q4
